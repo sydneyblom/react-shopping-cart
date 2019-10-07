79b6c803dbb88c258dc7720bfe9487ffc5a34ea2
Sydney Blom <sydneyblom@#gmail.com>
2019-10-07 14:24:38 -0500

    5 wrap cardcontext in app

--- src/App.js
+++ src/App.js
@@ -2,6 +2,11 @@ import React, { useState } from 'react';
 import { Route } from 'react-router-dom';
 import data from './data';
 
+
+import {ProductContext} from './contexts/ProductContext';
+import {CartContext} from './contexts/CartContext';
+
+
 // Components
 import Navigation from './components/Navigation';
 import Products from './components/Products';
@@ -17,17 +22,18 @@ function App() {
 
 	return (
 		<div className="App">
+		<CartContext.Provider value={cart}>
 			<ProductContext.Provider value={{ products, addItem }}>
-			<Navigation cart={cart} />
+				<Navigation cart={cart} />
 
 			{/* Routes */}
-			<Route exact path="/" component={Products} />
+				<Route exact path="/" component={Products} />
 
-			<Route
-				path="/cart"
-				render={() => <ShoppingCart cart={cart} />}
-			/>
+				<Route
+				path="/cart" component={ShoppingCart} />}
+
 			</ProductContext.Provider>
+		</CartContext.Provider>
 		</div>
 	);
 }
